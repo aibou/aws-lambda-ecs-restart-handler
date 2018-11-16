71b980acbaa70b67c6dbb2f60e61ee10b91245ec
aibou <aibou7251@gmail.com>
2018-11-16 12:34:02 +0900

    Implement restart_service

--- lambda_function.py
+++ lambda_function.py
@@ -1,6 +1,7 @@
 import boto3
+import json
 
-def lambda_handler(event, context):
+def stop_task_handler(event, context):
     if event.get('cluster_name') is None:
         raise NameError("cluster_name does not set")
 
@@ -12,7 +13,23 @@ def lambda_handler(event, context):
     
     for task_arn in tasks['taskArns']:
         ecs_client.stop_task(cluster=event.get('cluster_name'), task=task_arn)
+
+def restart_service_handler(event, context):
+    force_restart = False
+
+    if event.get('cluster_name') is None:
+        raise NameError("cluster_name does not set")
+
+    if event.get('service_name') is None:
+        raise NameError("service_name does not set")
     
+    if event.get('force_restart') is not None:
+        force_restart = json.load(event.get('force_restart'))
+
+    ecs_client = boto3.client("ecs", region_name="ap-northeast-1")
+    ecs_client.update_service(cluster=event.get('cluster_name'), serviceName=event.get('service_name'), forceNewDeployment=force_restart)
+
+def _validate_event_arguments(event):
 
 if __name__ == '__main__':
     event = {
@@ -20,4 +37,4 @@ if __name__ == '__main__':
         'service_name': 'rodgers-service'
     }
     context = {}
-    lambda_handler(event, context)
+    stop_task_handler(event, context)
